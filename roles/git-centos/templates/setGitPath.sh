f7261100d6b80b335374389b6e30359ccc46e0c0
stsykalovskyi <stsykalovskyi@gmail.com>
2018-02-19 19:02:00 +0200

    enhance git role for centos

---
+++ roles/git-centos/templates/setGitPath.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# set git path to latest installed git version 2.13.0 (/usr/local/git/bin/git)
+# instead of preinstalled git version 1.8.3 (/bin/git)
+
+export PATH=/usr/local/git/bin:$PATH
