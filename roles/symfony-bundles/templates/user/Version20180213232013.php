05472c11fc97761716dda8c02d2fcd5dbd9a7333
stsykalovskyi <stsykalovskyi@gmail.com>
2018-06-24 23:37:59 +0300

    fixes

--- roles/symfony-bundles/templates/user/Version20180213232013.php
+++ roles/symfony-bundles/templates/user/Version20180213232013.php
@@ -2,8 +2,8 @@
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -13,23 +13,51 @@ class Version20180213232013 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
 
-        $this->addSql('CREATE SEQUENCE gamehub.fos_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE {{ project_database }}.fos_user (id INT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(255) NOT NULL, first_name VARCHAR(64) DEFAULT NULL, last_name VARCHAR(64) DEFAULT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled BOOLEAN NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, confirmation_token VARCHAR(255) DEFAULT NULL, password_requested_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, roles TEXT NOT NULL, avatar VARCHAR(255) DEFAULT NULL, hash VARCHAR(24) DEFAULT NULL, facebook_id VARCHAR(255) DEFAULT NULL, facebook_access_token VARCHAR(255) DEFAULT NULL, google_id VARCHAR(255) DEFAULT NULL, google_access_token VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B892FC23A8 ON {{ project_database }}.fos_user (username_canonical)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8E7927C74 ON {{ project_database }}.fos_user (email)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8A0D96FBF ON {{ project_database }}.fos_user (email_canonical)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8C05FB297 ON {{ project_database }}.fos_user (confirmation_token)');
-        $this->addSql('COMMENT ON COLUMN {{ project_database }}.fos_user.roles IS \'(DC2Type:array)\'');
+        $this->addSql('CREATE SEQUENCE {{ database.schema }}.fos_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql(
+            'CREATE TABLE {{ database.schema }}.fos_user (
+                              id INT NOT NULL,
+                              username VARCHAR(180) NOT NULL,
+                              username_canonical VARCHAR(255) NOT NULL,
+                              first_name VARCHAR(64) DEFAULT NULL,
+                              last_name VARCHAR(64) DEFAULT NULL,
+                              email VARCHAR(180) NOT NULL,
+                              email_canonical VARCHAR(180) NOT NULL,
+                              enabled BOOLEAN NOT NULL,
+                              salt VARCHAR(255) DEFAULT NULL,
+                              password VARCHAR(255) NOT NULL,
+                              last_login TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
+                              confirmation_token VARCHAR(255) DEFAULT NULL,
+                              password_requested_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL,
+                              roles TEXT NOT NULL,
+                              avatar VARCHAR(255) DEFAULT NULL,
+                              hash VARCHAR(24) DEFAULT NULL,
+                              facebook_id VARCHAR(255) DEFAULT NULL,
+                              facebook_access_token VARCHAR(255) DEFAULT NULL,
+                              google_id VARCHAR(255) DEFAULT NULL,
+                              google_access_token VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))'
+        );
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B892FC23A8 ON {{ database.schema }}.fos_user (username_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8E7927C74 ON {{ database.schema }}.fos_user (email)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8A0D96FBF ON {{ database.schema }}.fos_user (email_canonical)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_356FE4B8C05FB297 ON {{ database.schema }}.fos_user (confirmation_token)');
+        $this->addSql('COMMENT ON COLUMN {{ database.schema }}.fos_user.roles IS \'(DC2Type:array)\'');
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'postgresql',
+            'Migration can only be executed safely on \'postgresql\'.'
+        );
 
-        $this->addSql('DROP SEQUENCE {{ project_database }}.fos_user_id_seq CASCADE');
-        $this->addSql('DROP TABLE {{ project_database }}.fos_user');
+        $this->addSql('DROP SEQUENCE {{ database.schema }}.fos_user_id_seq CASCADE');
+        $this->addSql('DROP TABLE {{ database.schema }}.fos_user');
     }
 }
